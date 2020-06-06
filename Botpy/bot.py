a91a6823a5e6fc55e2954111f4f14ef99fd11417
Tony <b3ntuz1@gmail.com>
2020-06-06 13:55:12 +0300

    form now addCmd() and BotCmds work as expected

--- Botpy/bot.py
+++ Botpy/bot.py
@@ -1,5 +1,5 @@
-from config import BotConfig as config
-from logger import Logger
+from Botpy.config import BotConfig as config
+from Botpy.logger import Logger
 
 logger = Logger.getLogger(Logger, "bot.py")
 logger.info("Starting")
@@ -10,7 +10,7 @@ class Bot:
     bot_cmd = {}  # bot command and there function
 
     def __init__(self):
-        from BotApi import getWebhookInfo, setWebhook
+        from Botpy.BotApi import getWebhookInfo, setWebhook
         # Check webhook
         webhook_info = getWebhookInfo()
         if('url' in webhook_info['result'].keys()):
@@ -35,18 +35,65 @@ class Bot:
         """ декоратор реєстрації команд для бота """
         def wrapped(func):
             self.bot_cmd[cmd] = func
+            return func
         return wrapped
 
 
-class BotCmds:
+class BotCmds(object):
     """ Команди бота """
 
     Bot = Bot()
 
+    def _execute(self, sql_query):
+        """ метод для роботи з бд. На вхід приймиє sql_query,
+        на виході поверне масив контежів
+        sql_query = str, sql query
+        """
+        import pymysql
+        conn = pymysql.connect(host="localhost", user="tony",
+                               password="password", db="testdb", charset="utf8")
+        try:
+            with conn.cursor() as cursor:
+                c = cursor.execute(sql_query)
+                print("c == " + str(c))
+                result = [row for row in cursor]
+                return result
+        except pymysql.Error as e:
+            logger.info("MySQL error: " + str(e))
+        finally:
+            conn.close()
+
+    def _parse_raw_data(self, raw_data, schema):
+        """ парсить сирі дані з бд. повертає list of dicts """
+        result = []
+
+        for row in raw_data:
+            i = 0
+            tmp = {}
+            for data in row:
+                tmp[schema[i]] = data
+                i += 1
+            result.append(tmp)
+        return result
+
     @Bot.addCmd('showme')
-    def showme(self):
+    def showme(self, pkm_name):
         """ покаже дані про покемона """
-        pass
+        # 1.сходити в бд за даними
+        sql_query = "SELECT * FROM pokedex WHERE pokemon_name='{}'".format(
+            pkm_name)
+        raw_data = self._execute(sql_query)
+        # 2. спарсити сирі дані.
+        schema = ['id', 'pokemon_id', 'pokemon_name', 'form', 'types',
+                  'base_attack', 'base_defense', 'base_stamina', 'file_id']
+        pretty_data = self._parse_raw_data(raw_data, schema)
+        pretty_data = pretty_data[0]
+        # 3. передати в BotApi.sendPhoto()
+        caption = "name: {}\ntype: {}\nbase atk: {}\n -*- def: {}\n -*- sta: {}"
+        caption = caption.format(pretty_data['pokemon_name'], pretty_data['types'], pretty_data['base_attack'],
+                                 pretty_data['base_defense'], pretty_data['base_stamina'])
+
+        print(caption)
 
     @Bot.addCmd('raidbosses')
     def raidbosses(self):
--- Botpy/config.py
+++ Botpy/config.py
@@ -1,7 +1,7 @@
 from os import getenv
 
+
 class BotConfig:
     tg_url = "https://api.telegram.org/"
     tg_webhook = "https://26da12afac08.ngrok.io"
-    tg_api_key = getenv("tg_api_key")
-    
+    tg_api_key = "1012063969:AAHhiUHhptVRDrCMlOSKAxmCIehn49nfRMU"
