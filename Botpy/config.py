f7eba4f95394a3dbdaabf8a860946fb4631a903d
dude_from_nirvana <b3ntuz1@gmail.com>
2020-07-13 15:02:06 +0300

    Update config.py

--- Botpy/config.py
+++ Botpy/config.py
@@ -3,5 +3,5 @@ from os import getenv
 
 class BotConfig:
     tg_url = "https://api.telegram.org/"
-    tg_webhook = "https://26da12afac08.ngrok.io"
-    tg_api_key = "1012063969:AAHhiUHhptVRDrCMlOSKAxmCIehn49nfRMU"
+    tg_webhook = ""
+    tg_api_key = ""
